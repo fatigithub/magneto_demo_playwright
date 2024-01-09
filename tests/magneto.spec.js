eae8e4c3e0d97b07e29feb46ab6db182034e3e63
fatigithub <kassimiftaimazahra68@gmail.com>
2024-01-09 17:37:45 +0100

    create account

--- tests/magneto.spec.js
+++ tests/magneto.spec.js
@@ -1,16 +1,28 @@
-import { test, expect } from '@playwright/test';
-const baseUrl='https://magento.softwaretestingboard.com/'
+import { test, expect } from "@playwright/test";
 let context;
 let page;
-test.beforeAll(async ({browser}) => {
-     context = await browser.newContext();
-     page = await context.newPage();
-     await page.goto(baseUrl);
-  });
+test.beforeAll(async ({ browser }) => {
+  context = await browser.newContext();
+  page = await context.newPage();
+  await page.goto("/");
+});
 
-
-test('should creat account', async ({}) => {
-  await expect(page.getByRole('link',{name:"Create an Account"})).toHaveAttribute('href','https://magento.softwaretestingboard.com/customer/account/create/');
-  await page.getByRole('link',{name:"Create an Account"}).click();
- 
-})
+test("should create account", async ({}) => {
+  await expect(
+    page.getByRole("link", { name: "Create an Account" })
+  ).toHaveAttribute(
+    "href",
+    "https://magento.softwaretestingboard.com/customer/account/create/"
+  );
+  await page.getByRole("link", { name: "Create an Account" }).click();
+  await expect(page).toHaveURL("/customer/account/create/");
+  await page.getByTitle("First Name").fill("demoTesteur");
+  await page.getByTitle("Last Name").fill("demoTesteur");
+  await page.getByTitle("Email").fill("demoTesteur@gmail.com");
+  await page.locator("#password").fill("Azert1@/:*");
+  await page.getByTitle("Confirm Password").fill("Azert1@/:*");
+  await page.getByRole("button", { name: "Create an Account" }).click();
+  await expect(
+    page.getByText("Thank you for registering with Main Website Store.")
+  ).toHaveText("Thank you for registering with Main Website Store.");
+});
